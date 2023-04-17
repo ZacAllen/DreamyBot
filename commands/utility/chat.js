10ddb0856c9f91dd05a0a6958394ab7fd4bb6e8b
Zach Allen <zacharysallen97@gmail.com>
2023-04-17 16:17:21 -0400

    added split prompt result

--- commands/utility/chat.js
+++ commands/utility/chat.js
@@ -21,22 +21,50 @@ module.exports = {
     await interaction.reply('Let me think about it...')
     let completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: `${interaction.options.getString('message')}` }],
+      messages: [{ role: 'system', content: `${interaction.options.getString('message')}` }],
       // model: 'text-davinci-003',
       // prompt: `${interaction.options.getString('message')}`,
     })
 
     console.log('*** Your response is: ', completion?.data?.choices[0]?.message)
 
-    await interaction
-      .editReply(
-        `**"${interaction.options.getString('message')}":**
-     \`\`\`fix
-    ${completion?.data?.choices[0]?.message?.content}
-    \`\`\` `,
-      )
-      .catch((err) => {
-        interaction.editReply('Ah fuck, I have encountered a problem generating your response.')
+    const result = completion?.data?.choices[0]?.message?.content
+    let splitResult1 = null
+    let splitResult2 = null
+    let tooLong = false
+
+    if (result.length > 1999) {
+      console.log('*** RESULT LENGTH', result.length)
+      tooLong = true
+    }
+
+    /* I initially wrote this thinking tooLong would be reused someplace else, excuse the redundancy */
+    if (tooLong) {
+      splitResult1 = result.slice(0, result.length / 2)
+      splitResult2 = result.slice(result.length / 2, result.length)
+      await interaction.editReply(`**"${interaction.options.getString('message')}":**
+      \`\`\`fix
+      ${splitResult1}-
+      (Part 1)
+      \`\`\``)
+      await interaction.channel.send({
+        content: `\`\`\`fix
+      -${splitResult2}
+      (Part 2)
+      \`\`\``,
       })
+    } else {
+      await interaction
+        .editReply(
+          `**"${interaction.options.getString('message')}":**
+       \`\`\`fix
+      ${result}
+      \`\`\` `,
+        )
+        .catch((err) => {
+          console.log(err)
+          interaction.editReply('Ah fuck, I have encountered a problem generating your response.')
+        })
+    }
   },
 }
