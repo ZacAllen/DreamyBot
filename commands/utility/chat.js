72bb402550199abdb1bd6417e08252438aecb5e5
Zach Allen <zacharysallen97@gmail.com>
2023-05-23 17:34:09 -0400

    Update chat.js

--- commands/utility/chat.js
+++ commands/utility/chat.js
@@ -1,81 +1,97 @@
-const { SlashCommandBuilder } = require('discord.js')
-const { config } = require('dotenv')
-const { Configuration, OpenAIApi } = require('openai')
-const openaikey = process.env.GPT_API_KEY
+const { SlashCommandBuilder } = require("discord.js");
+const { config } = require("dotenv");
+const { Configuration, OpenAIApi } = require("openai");
+const openaikey = process.env.GPT_API_KEY;
 
 const configuration = new Configuration({
   apiKey: openaikey,
-})
+});
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('chat')
-    .setDescription('what if the robot go crazy they might take over the world')
+    .setName("chat")
+    .setDescription("what if the robot go crazy they might take over the world")
     .addStringOption((option) =>
-      option.setName('message').setDescription('What would you like to ask DreamyBot?').setRequired(true),
+      option
+        .setName("message")
+        .setDescription("What would you like to ask DreamyBot?")
+        .setRequired(true)
     ),
 
   async execute(interaction) {
-    await interaction.reply('Let me think about it...')
+    await interaction.reply("Let me think about it...");
     let completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'system', content: `${interaction.options.getString('message')}` }],
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `${interaction.options.getString("message")}`,
+        },
+      ],
       // model: 'text-davinci-003',
       // prompt: `${interaction.options.getString('message')}`,
-    })
+    });
 
-    console.log('*** Your response is: ', completion?.data?.choices[0]?.message)
+    console.log(
+      "*** Your response is: ",
+      completion?.data?.choices[0]?.message
+    );
 
-    const result = completion?.data?.choices[0]?.message?.content
-    let splitResult = []
-    let tooLong = false
+    const result = completion?.data?.choices[0]?.message?.content;
+    let splitResult = [];
+    let tooLong = false;
 
     //check if result is longer than discord 2000 count message limit
-    if (result.length > 1999) {
-      console.log('*** RESULT LENGTH', result.length)
-      tooLong = true
-      let startIndex = 0
+    if (result.length > 1200) {
+      console.log("*** RESULT LENGTH", result.length);
+      tooLong = true;
+      let startIndex = 0;
       for (var i = 0; i < result.length; i++) {
-        if (i % 1999 == 0) {
-          splitResult.push(result.substring(startIndex, i))
-          startIndex = i
+        if (i % 1200 == 0 && i != 0) {
+          splitResult.push(result.substring(startIndex, i));
+          startIndex = i;
         }
         //once i reaches end, add remaining to splitResult
-        i == result.length - 1 && splitResult.push(result.substring(startIndex, i))
+        i == result.length - 1 &&
+          splitResult.push(result.substring(startIndex, i));
       }
     }
 
     /* I initially wrote this thinking tooLong would be reused someplace else, excuse the redundancy */
     if (tooLong) {
-      await interaction.editReply(`**"${interaction.options.getString('message')}":**
+      await interaction.editReply(`**"${interaction.options.getString(
+        "message"
+      )}":**
       \`\`\`fix
       ${splitResult[0]}-
       (Part 1)
-      \`\`\``)
+      \`\`\``);
       splitResult.forEach((section, index) => {
         if (section !== splitResult[0]) {
           interaction.channel.send({
             content: `\`\`\`fix
-          -${splitResult2}
+          -${splitResult[index]}
           (Part ${index + 1})
           \`\`\``,
-          })
+          });
         }
-      })
+      });
     } else {
       await interaction
         .editReply(
-          `**"${interaction.options.getString('message')}":**
+          `**"${interaction.options.getString("message")}":**
        \`\`\`fix
       ${result}
-      \`\`\` `,
+      \`\`\` `
         )
         .catch((err) => {
-          console.log(err)
-          interaction.editReply('Ah fuck, I have encountered a problem generating your response.')
-        })
+          console.log(err);
+          interaction.editReply(
+            "Ah fuck, I have encountered a problem generating your response."
+          );
+        });
     }
   },
-}
+};
