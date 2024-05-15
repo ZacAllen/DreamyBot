2eaed3443e74281f60d76d45315a6693bd544c53
Zach Allen <zallen@roomstogo.com>
2024-05-15 16:06:52 -0400

    add image orientation

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-require("dotenv").config(); //to start process from .env file
-const fs = require("fs");
-const path = require("path");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-const { AudioManager } = require("discordaudio");
-const { EmbedBuilder } = require("discord.js");
-const ytstream = require("yt-stream");
+require('dotenv').config() //to start process from .env file
+const fs = require('fs')
+const path = require('path')
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
+const { AudioManager } = require('discordaudio')
+const { EmbedBuilder } = require('discord.js')
+const ytstream = require('yt-stream')
 
 const client = new Client({
   intents: [
@@ -14,40 +14,40 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildVoiceStates,
   ],
-});
+})
 
 // attaching a .commands property to the client instance so that we can access commands in other files
-client.commands = new Collection();
+client.commands = new Collection()
 
-const foldersPath = path.join(__dirname, "commands"); //find path to commands folder and files
-const commandFolders = fs.readdirSync(foldersPath);
+const foldersPath = path.join(__dirname, 'commands') //find path to commands folder and files
+const commandFolders = fs.readdirSync(foldersPath)
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js")); //filter non js files
+  const commandsPath = path.join(foldersPath, folder)
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js')) //filter non js files
   for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
+    const filePath = path.join(commandsPath, file)
+    const command = require(filePath)
     // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command)
     } else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
     }
   }
 }
 
 // Reading event files
-const eventsPath = path.join(__dirname, "events");
-const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
+const eventsPath = path.join(__dirname, 'events')
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'))
 
 for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
+  const filePath = path.join(eventsPath, file)
+  const event = require(filePath)
   if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
+    client.once(event.name, (...args) => event.execute(...args))
   } else {
-    client.on(event.name, (...args) => event.execute(...args));
+    client.on(event.name, (...args) => event.execute(...args))
   }
 }
 
@@ -58,254 +58,254 @@ for (const file of eventFiles) {
 
 // --------------------------------------------------MUSIC COMMANDS--------------------------------------------------------------------
 
-const connections = new Map();
+const connections = new Map()
 
-let audioManager = new AudioManager();
+let audioManager = new AudioManager()
 
-const playPrefix = process.env.COMMAND_PREFIX;
+const playPrefix = process.env.COMMAND_PREFIX
 
-client.on("messageCreate", async (message) => {
-  if (message.author.bot || message.channel.type === `DM`) return;
+client.on('messageCreate', async (message) => {
+  if (message.author.bot || message.channel.type === `DM`) return
 
-  if (!message.content.startsWith(playPrefix)) return;
+  if (!message.content.startsWith(playPrefix)) return
 
-  let args = message.content.substring(playPrefix.length).split(" ");
+  let args = message.content.substring(playPrefix.length).split(' ')
 
-  const vc = connections.get(message.guild.members.me.voice.channel?.id);
+  const vc = connections.get(message.guild.members.me.voice.channel?.id)
 
-  const includeCommands = [`play`];
+  const includeCommands = [`play`]
 
-  let playError = false;
+  let playError = false
 
   // Only fetch title for commands that need it!
   let videoTitle =
-    includeCommands.includes(args[0]) && !args[1].includes("/playlist")
+    includeCommands.includes(args[0]) && !args[1].includes('/playlist')
       ? await ytstream
           .getInfo(args[1])
           .then((info) => {
-            return info.title;
+            return info.title
           })
           .catch((err) => {
-            if (args[1]) console.log("Error playing YT link!", args[1], err);
-            playError = true;
+            if (args[1]) console.log('Error playing YT link!', args[1], err)
+            playError = true
             message.channel.send({
               content: `${err} - Please provide a valid video link.`,
-            });
-            return null;
+            })
+            return null
           })
-      : null;
-  if (includeCommands.includes(args[0]) && args[1].includes("/playlist")) {
+      : null
+  if (includeCommands.includes(args[0]) && args[1].includes('/playlist')) {
     videoTitle = await ytstream
       .getPlaylist(args[1])
       .then((info) => {
         // console.log("Playlist count", info.videos.length);
-        return info.title;
+        return info.title
       })
       .catch((err) => {
-        playError = true;
+        playError = true
         message.channel.send({
           content: `${err} - Please provide a valid playlist link.`,
-        });
-        return null;
-      });
+        })
+        return null
+      })
   }
 
   switch (args[0].toLowerCase()) {
-    case "play":
+    case 'play':
       if (!message.member.voice.channel && !message.guild.members.me.voice.channel)
         return message.channel.send({
           content: `Please join a voice channel in order to play a song!`,
-        });
-      if (!args[1]) return message.channel.send({ content: `Please provide a song` });
-      const uvc = message.member.voice.channel || message.guild.members.me.voice.channel;
+        })
+      if (!args[1]) return message.channel.send({ content: `Please provide a song` })
+      const uvc = message.member.voice.channel || message.guild.members.me.voice.channel
 
       if (!audioManager) {
-        audioManager = new AudioManager();
+        audioManager = new AudioManager()
       }
       audioManager
         .play(uvc, args[1], {
-          quality: "high",
-          audiotype: "arbitrary",
+          quality: 'high',
+          audiotype: 'arbitrary',
           volume: 10,
         })
         .then((queue) => {
-          if (playError) return;
-          connections.set(uvc.id, uvc);
+          if (playError) return
+          connections.set(uvc.id, uvc)
           if (queue === false)
             message.channel.send({
               content: `Now playing  **${videoTitle ? videoTitle : args[1]}**`,
-            });
+            })
           else
             message.channel.send({
-              content: `${videoTitle ? videoTitle : "Your song"} has been added to the queue!`,
-            });
+              content: `${videoTitle ? videoTitle : 'Your song'} has been added to the queue!`,
+            })
         })
         .catch((err) => {
-          console.log(err);
+          console.log(err)
           message.channel.send({
             content: `There was an error while trying to connect to the voice channel!`,
-          });
-        });
-      break;
-    case "pause":
+          })
+        })
+      break
+    case 'pause':
       if (!vc)
         return message.channel.send({
           content: `There is currently nothing playing!`,
-        });
-      audioManager.pause(vc);
-      message.channel.send({ content: `Player paused.` });
-      break;
-    case "resume":
+        })
+      audioManager.pause(vc)
+      message.channel.send({ content: `Player paused.` })
+      break
+    case 'resume':
       {
         if (!vc)
           return message.channel.send({
             content: `There is currently nothing playing!`,
-          });
-        const queue = audioManager.queue(vc);
-        audioManager.resume(vc);
-        message.channel.send({ content: `Resuming playback of **${queue[0]?.title}.**` });
+          })
+        const queue = audioManager.queue(vc)
+        audioManager.resume(vc)
+        message.channel.send({ content: `Resuming playback of **${queue[0]?.title}.**` })
       }
-      break;
-    case "skip":
+      break
+    case 'skip':
       {
         if (!vc)
           return message.channel.send({
             content: `There is currently nothing playing!`,
-          });
-        const queue = audioManager.queue(vc);
+          })
+        const queue = audioManager.queue(vc)
         audioManager
           .skip(vc)
           .then(() => {
-            message.channel.send({ content: `Skipping song **${queue[0]?.title || ""}.**` });
+            message.channel.send({ content: `Skipping song **${queue[0]?.title || ''}.**` })
           })
           .catch((err) => {
             message.channel.send({
               content: `There was an error while skipping the song!`,
-            });
-          });
+            })
+          })
       }
-      break;
-    case "loop":
+      break
+    case 'loop':
       {
         if (!vc)
           return message.channel.send({
             content: `There is currently nothing playing!`,
-          });
-        const queue = audioManager.queue(vc);
-        audioManager.loop(vc, audioManager?.looptypes?.loop);
-        message?.channel.send({ content: `Looping current song ${queue[0]?.title || ""}.` });
+          })
+        const queue = audioManager.queue(vc)
+        audioManager.loop(vc, audioManager?.looptypes?.loop)
+        message?.channel.send({ content: `Looping current song ${queue[0]?.title || ''}.` })
       }
-      break;
-    case "stop":
+      break
+    case 'stop':
       if (!vc)
         return message.channel.send({
           content: `There is currently nothing playing!`,
-        });
-      audioManager.stop(vc);
-      message.channel.send({ content: `Playback stopped!` });
-      break;
-    case "queue":
+        })
+      audioManager.stop(vc)
+      message.channel.send({ content: `Playback stopped!` })
+      break
+    case 'queue':
       {
         if (!vc)
           return message.channel.send({
             content: `There is currently nothing playing!`,
-          });
+          })
         const queue = audioManager.queue(vc).reduce((text, song, index) => {
           if (index > 50) {
-            return text;
+            return text
           } else if (index > 49) {
-            text += `\n...`;
-            return text;
+            text += `\n...`
+            return text
           }
-          if (song.title) text += `\n**[${index + 1}]** ${song.title}`;
-          else text += `\n**[${index + 1}]** ${song.url}`;
-          return text;
-        }, `__**QUEUE**__`);
+          if (song.title) text += `\n**[${index + 1}]** ${song.title}`
+          else text += `\n**[${index + 1}]** ${song.url}`
+          return text
+        }, `__**QUEUE**__`)
         const queueEmbed = new EmbedBuilder()
           .setColor(`Blurple`)
           .setTitle(`Queue: [${audioManager.queue(vc).length}] Songs`)
-          .setDescription(queue);
+          .setDescription(queue)
         if (queueEmbed) {
-          message.channel.send({ embeds: [queueEmbed] });
+          message.channel.send({ embeds: [queueEmbed] })
         } else {
-          message.channel.send({ content: `There was an error while reading the queue!` });
+          message.channel.send({ content: `There was an error while reading the queue!` })
         }
       }
 
-      break;
-    case "volume":
+      break
+    case 'volume':
       if (!vc)
         return message.channel.send({
           content: `There is currently nothing playing!`,
-        });
-      if (!args[1]) return message.channel.send({ content: `Please provide the volume` });
+        })
+      if (!args[1]) return message.channel.send({ content: `Please provide the volume` })
       if (Number(args[1]) < 1 || Number(args[1]) > 10)
         return message.channel.send({
           content: `Please provide a volume between 1-10`,
-        });
-      audioManager.volume(vc, Number(args[1]));
-      break;
-    case "shuffle":
+        })
+      audioManager.volume(vc, Number(args[1]))
+      break
+    case 'shuffle':
       if (!vc)
         return message.channel.send({
           content: `There is currently nothing playing!`,
-        });
-      audioManager.shuffle(vc);
+        })
+      audioManager.shuffle(vc)
       message.channel.send({
         content: `The queue has successfully been shufffled`,
-      });
-      break;
-    case "clear":
+      })
+      break
+    case 'clear':
       if (!vc)
         return message.channel.send({
           content: `There is no queue!`,
-        });
-      audioManager.clearqueue(vc);
+        })
+      audioManager.clearqueue(vc)
       message.channel.send({
         content: `The queue has successfully been cleared`,
-      });
-      break;
-    case "diag":
+      })
+      break
+    case 'diag':
       if (!vc)
         return message.channel.send({
           content: `There is no queue!`,
-        });
-      const queue = audioManager.diagnostic(vc);
-      const current = queue[0];
+        })
+      const queue = audioManager.diagnostic(vc)
+      const current = queue[0]
 
-      var msg = "```json\n{";
+      var msg = '```json\n{'
       for (var key in current) {
         if (current.hasOwnProperty(key)) {
-          msg = msg + '\n "' + key + '": "' + JSON.stringify(current[key], null, " ") + '",';
+          msg = msg + '\n "' + key + '": "' + JSON.stringify(current[key], null, ' ') + '",'
         }
       }
-      msg = msg.substring(0, msg.length - 1);
-      msg = msg + "\n}```";
+      msg = msg.substring(0, msg.length - 1)
+      msg = msg + '\n}```'
 
-      console.log("Stuff", queue);
+      console.log('Stuff', queue)
       message.channel.send({
-        content: "Current song data: " + msg,
-      });
+        content: 'Current song data: ' + msg,
+      })
   }
-});
+})
 
-client.on("voiceStateUpdate", async (oldState, newState) => {
-  if (newState?.id === "1094140206367653940" && audioManager) {
+client.on('voiceStateUpdate', async (oldState, newState) => {
+  if (newState?.id === '1094140206367653940' && audioManager) {
     //check if bot left vc
-    let error = false;
+    let error = false
     if (newState?.channelId == null && oldState?.channelId != null) {
       try {
         //destroy audiomanager and end playback
         if (audioManager) {
-          audioManager.destroy();
+          audioManager.destroy()
         }
       } catch (err) {
         // console.log("*** AudioManager Destroy error?", err);
-        error = true;
+        error = true
       }
     }
   }
-});
+})
 
 // --------------------------------------------------MESSAGE EMBED FUNCS--------------------------------------------------------------------
 
@@ -313,52 +313,52 @@ client.on("voiceStateUpdate", async (oldState, newState) => {
  * Create vxtwitter link from non-embedable native links. Replies to user with vx link.
  * @params { object } message
  */
-client.on("messageCreate", async (message) => {
-  const mess = message.content;
-  const twitterLink = ["https://x.com", "https://twitter.com"].find((link) => mess.includes(link));
+client.on('messageCreate', async (message) => {
+  const mess = message.content
+  const twitterLink = ['https://x.com', 'https://twitter.com'].find((link) => mess.includes(link))
 
   if (twitterLink && !message.author.bot) {
-    message.author.bot ? false : message.suppressEmbeds(true), message.suppressEmbeds(true);
+    message.author.bot ? false : message.suppressEmbeds(true), message.suppressEmbeds(true)
     message.reply({
       /*
       Empty character unicode 
       */
-      content: `[⠀](${mess.replace(twitterLink, "https://vxtwitter.com")})`,
-    });
+      content: `[⠀](${mess.replace(twitterLink, 'https://vxtwitter.com')})`,
+    })
   }
-});
+})
 
 /**
  * Create tiktok embed for tiktok videos. Replies to user with vx embed and suppresses native embed.
  * @params { object } message
  */
-client.on("messageCreate", async (message) => {
-  const mess = message.content;
-  const tiktokLink = ["https://tiktok.com", "https://www.tiktok.com"].find((link) => mess.includes(link));
+client.on('messageCreate', async (message) => {
+  const mess = message.content
+  const tiktokLink = ['https://tiktok.com', 'https://www.tiktok.com'].find((link) => mess.includes(link))
 
   if (tiktokLink) {
-    message.author.bot ? false : message.suppressEmbeds(true), message.suppressEmbeds(true);
+    message.author.bot ? false : message.suppressEmbeds(true), message.suppressEmbeds(true)
     message.reply({
-      content: `[⠀](${mess.replace(tiktokLink, "https://vxtiktok.com")})`,
-    });
+      content: `[⠀](${mess.replace(tiktokLink, 'https://vxtiktok.com')})`,
+    })
   }
-});
+})
 
 /**
  * Create reddit embed for reddit links. Replies to user with rxddit link, and suppresses native embed.
  * @params { object } message
  */
-client.on("messageCreate", async (message) => {
-  const mess = message.content;
-  const redditLink = ["https://reddit.com", "https://www.reddit.com"].find((link) => mess.includes(link));
+client.on('messageCreate', async (message) => {
+  const mess = message.content
+  const redditLink = ['https://reddit.com', 'https://www.reddit.com'].find((link) => mess.includes(link))
 
   if (redditLink) {
-    message.author.bot ? false : message.suppressEmbeds(true), message.suppressEmbeds(true);
+    message.author.bot ? false : message.suppressEmbeds(true), message.suppressEmbeds(true)
     message.reply({
-      content: `[⠀](${mess.replace(redditLink, "https://rxddit.com")})`,
-    });
-    console.log("*** Suppressing Reddit Embed", message);
+      content: `[⠀](${mess.replace(redditLink, 'https://rxddit.com')})`,
+    })
+    console.log('*** Suppressing Reddit Embed', message)
   }
-});
+})
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
--- commands/utility/image.js
+++ commands/utility/image.js
@@ -1,62 +1,73 @@
-const { SlashCommandBuilder } = require("discord.js");
-const { EmbedBuilder } = require("discord.js");
-const { OpenAI } = require("openai");
-const openaikey = process.env.GPT_API_KEY;
+const { SlashCommandBuilder } = require('discord.js')
+const { EmbedBuilder } = require('discord.js')
+const { OpenAI } = require('openai')
+const openaikey = process.env.GPT_API_KEY
 
 const openai = new OpenAI({
   apiKey: openaikey,
-});
+})
 
-const dreamyImage = `https://i.kym-cdn.com/entries/icons/original/000/042/513/dreamybull.jpg`;
+const dreamyImage = `https://i.kym-cdn.com/entries/icons/original/000/042/513/dreamybull.jpg`
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("image")
-    .setDescription("This is REAL art!")
+    .setName('image')
+    .setDescription('This is REAL art!')
     .addStringOption((option) =>
-      option.setName("message").setDescription("Ask DreamyBot to create anything you can imagine 😲").setRequired(true)
+      option.setName('message').setDescription('Ask DreamyBot to create anything you can imagine 😲').setRequired(true),
     ),
 
   async execute(interaction) {
-    await interaction.reply("Let me think about it...");
+    await interaction.reply('Let me think about it...')
+    let size = '1024x1024'
+    let message = interaction.options.getString('message')
+    if (message.includes('{landscape}')) {
+      size = '1792x1024'
+      message = message.replace('{landscape}', '')
+    } else if (message.includes('{portrait}')) {
+      size = '1024x1792'
+      message = message.replace('{portrait}', '')
+    }
     let response = await openai.images
       .generate({
-        model: "dall-e-3",
-        prompt: `${interaction.options.getString("message")}`,
+        model: 'dall-e-3',
+        prompt: message,
         n: 1,
-        size: "1024x1024",
+        size: size,
       })
       .catch((err) => {
-        if (err.error.message.includes("safety system")) {
+        if (err.error.message.includes('safety system')) {
           interaction
             .editReply(
               `OpenAI did NOT like that prompt: \n\n"**${interaction.options.getString(
-                "message"
-              )}**"\n\nApparently '*Your request was rejected as a result of our safety system.*' Try being more SAFE!`
+                'message',
+              )}**"\n\nApparently '*Your request was rejected as a result of our safety system.*' Try being more SAFE!`,
             )
             .catch((err) => {
-              interaction.editReply(err);
-            });
+              interaction.editReply(err)
+            })
         } else {
-          interaction.editReply(`"**${interaction.options.getString("message")}**"\n\nIdk what tf you talking bout dawg`);
+          interaction.editReply(
+            `"**${interaction.options.getString('message')}**"\n\nIdk what tf you talking bout dawg`,
+          )
         }
-      });
-    image_url = response.data[0].url;
+      })
+    image_url = response.data[0].url
 
     const imageEmbed = new EmbedBuilder()
       .setColor(0x0099ff)
-      .setTitle("Your image")
+      .setTitle('Your image')
       // .setURL("https://discord.js.org/")
-      .setAuthor({ name: "DreamyBot", iconURL: dreamyImage })
-      .setDescription(`"${interaction.options.getString("message")}"`)
+      .setAuthor({ name: 'DreamyBot', iconURL: dreamyImage })
+      .setDescription(`"${interaction.options.getString('message')}"`)
       // .setThumbnail(dreamyImage)
       .setImage(image_url)
-      .setTimestamp();
+      .setTimestamp()
     // .setFooter({ text: "Some footer text here", iconURL: "https://i.imgur.com/AfFp7pu.png" });
 
     await interaction.editReply({ embeds: [imageEmbed] }).catch((err) => {
-      console.log(err);
-      interaction.editReply("Ah fuck, I have encountered a problem generating your response.");
-    });
+      console.log(err)
+      interaction.editReply('Ah fuck, I have encountered a problem generating your response.')
+    })
   },
-};
+}
