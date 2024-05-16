58c045f80646b26416da33d96aa69d77f6ba611b
Zach Allen <zallen@roomstogo.com>
2024-05-16 15:49:05 -0400

    image command description

--- commands/utility/image.js
+++ commands/utility/image.js
@@ -14,7 +14,12 @@ module.exports = {
     .setName('image')
     .setDescription('This is REAL art!')
     .addStringOption((option) =>
-      option.setName('message').setDescription('Ask DreamyBot to create anything you can imagine 😲').setRequired(true),
+      option
+        .setName('message')
+        .setDescription(
+          'Ask DreamyBot to create anything you can imagine 😲 (use {landscape} or {portrait} to specify dimensions)',
+        )
+        .setRequired(true),
     ),
 
   async execute(interaction) {
